3a693ec1b466b90071e02ae2f1c36a52461324da
Tech Code <129946873+dyapan33@users.noreply.github.com>
2024-04-20 16:24:02 +0100

    Initial commit

--- Third-Party/Edit.py
+++
@@ -1,9 +0,0 @@
-import os as o
-import sys as sy
-
-
-o.system('git clone -b master --recursive https://github.com/SergiusTheBest/plog.git Logging/')
-
-sy.copyright("(C) 2023-2024 Almond. All Rights Reserved.")
-
-
